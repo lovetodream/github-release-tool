c59a52d755427d6ca71b0e248a374989d5299568
Timo <38291523+lovetodream@users.noreply.github.com>
2023-12-30 11:12:19 +0100

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "github-release-tool",
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "GitHubReleaseTool",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ]
+        ),
+    ]
+)
---
+++ Sources/GitHubReleaseTool.swift
@@ -0,0 +1,8 @@
+import ArgumentParser
+
+@main
+struct GitHubReleaseTool: ParsableCommand {
+    mutating func run() throws {
+        print("Hello, world!")
+    }
+}
